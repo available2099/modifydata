fdb5a1557c220574e78888c40648a3e6f84f1047
hao2.li <hao873835932>
2020-09-09 17:23:09 +0800

    Dockerfile修改

---
+++ start.sh
@@ -0,0 +1,28 @@
+ #!/usr/bin/env bash
+ cd /var/local/jdhelper/con
+ #删除容器
+  docker rm -f $(docker ps -a | grep jdhelperconsumer | awk '{print $1}')
+  #删除镜像
+  docker rmi jdhelperconsumer
+
+  #打包镜像
+  docker build -t jdhelperconsumer .
+
+  #启动容器
+  docker run -d --net=host --name=jdhelperconsumer jdhelperconsumer
+
+  echo "$var启动完成"
+
+  cd /var/local/jdhelper/pro
+ #删除容器
+  docker rm -f $(docker ps -a | grep jdhelperprovider | awk '{print $1}')
+  #删除镜像
+  docker rmi jdhelperprovider
+
+  #打包镜像
+  docker build -t jdhelperprovider .
+
+  #启动容器
+  docker run -d --net=host --name=jdhelperprovider jdhelperprovider
+
+  echo "$var启动完成"
